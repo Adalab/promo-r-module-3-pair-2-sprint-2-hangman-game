90ff84fcf89ad17b54a732a50f42ef296a330cdc
Luciakestel <luciakestelpires@gmail.com>
2022-11-30 10:53:34 +0100

    add form component

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import Dummy from './Dummy'
 import SolutionLetters from './SolutionLetters'
 import ErrorLetters from './ErrorLetters'
+import Form from './Form'
 
 
 // api
@@ -27,21 +28,8 @@ function App() {
 
   // events
 
-  const handleKeyDown = (ev) => {
-    // Sabrías decir para qué es esta línea
-    ev.target.setSelectionRange(0, 1);
-  };
 
-  const handleChange = (ev) => {
-    let re = /^[a-zA-ZñÑá-úÁ-Ú´]$/; //add regular pattern 
-    if (re.test(ev.target.value) || ev.target.value === '') {
-      handleLastLetter(ev.target.value);
-    }
-  };
 
-  const handleSubmit = (ev) => {
-    ev.preventDefault();
-  };
 
   const getNumberOfErrors = () => {
     const errorLetters = userLetters.filter(
@@ -49,10 +37,6 @@ function App() {
     );
     return errorLetters.length;
   };
-
-
-
-
 
   const handleLastLetter = (value) => {
     value = value.toLocaleLowerCase();
@@ -71,23 +55,7 @@ function App() {
         <section>
           <SolutionLetters word={word} userLetters={userLetters}/>
          <ErrorLetters  word={word} userLetters={userLetters}/>
-          <form className='form' onSubmit={handleSubmit}>
-            <label className='title' htmlFor='last-letter'>
-              Escribe una letra:
-            </label>
-            <input
-              autoFocus
-              autoComplete='off'
-              className='form__input'
-              maxLength='1'
-              type='text'
-              name='last-letter'
-              id='last-letter'
-              value={lastLetter}
-              onKeyDown={handleKeyDown}
-              onChange={handleChange}
-            />
-          </form>
+          <Form lastLetter={lastLetter} handleLastLetter={handleLastLetter} />
         </section>
         <Dummy numberOfErrors={getNumberOfErrors()}/>
       </main>
